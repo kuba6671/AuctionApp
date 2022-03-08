c06615f9afe03b634ac2ce14b96ea0f8c428c5fb
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2022-03-08 23:54:31 +0100

    Added sales panel and logo

--- AuctionApp/MVVM/ViewModel/LoginViewModel.cs
+++ AuctionApp/MVVM/ViewModel/LoginViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using AuctionApp.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace AuctionApp.MVVM.ViewModel
 {
     class LoginViewModel
     {
+
+        public RelayCommand CloseCommand { get; set; }
+
+
+        public LoginViewModel()
+        {
+            CloseCommand = new RelayCommand(o =>
+            {
+                Application.Current.Shutdown();
+            });
+        }
     }
 }
