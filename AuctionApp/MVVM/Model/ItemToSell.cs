166f6c39fcc24d95dbaebe37215e3c9eefa231bf
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2022-03-15 23:43:23 +0100

    Added handling of addPhoto button in SellView

--- AuctionApp/MVVM/Model/ItemToSell.cs
+++ AuctionApp/MVVM/Model/ItemToSell.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace AuctionApp.MVVM.Model
 {
@@ -15,11 +16,11 @@ namespace AuctionApp.MVVM.Model
         private String name;
         private String username;
         private double price;
-        private Image itemImage;
+        private ImageSource itemImage;
 
-        public ItemToSell(string username, Image itemImage, string category, string size, string state, double price, string name)
+        public ItemToSell(ImageSource itemImage, string category, string size, string state, double price, string name)
         {
-            this.username = username;
+            this.username = User.getUsername();
             this.size = size;
             this.state = state;
             this.category = category;
@@ -50,7 +51,7 @@ namespace AuctionApp.MVVM.Model
             return price;
         }
 
-        public Image getImage()
+        public ImageSource getImage()
         {
             return itemImage;
         }
--- AuctionApp/MVVM/ViewModel/SellViewModel.cs
+++ AuctionApp/MVVM/ViewModel/SellViewModel.cs
@@ -1,40 +1,61 @@
 ﻿using AuctionApp.Core;
 using AuctionApp.MVVM.Model;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Controls;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace AuctionApp.MVVM.ViewModel
 {
     class SellViewModel : ObservableObject
     {
         public RelayCommand addCommand { get; set; }
+        public RelayCommand addPhotoCommand { get; set; }
+
 
         public SellViewModel()
         {
             addCommand = new RelayCommand(o =>
             {
                 string categoryString = category.ToString();
-                Trace.WriteLine("categoryString = " + categoryString);
-                /*string finalCategoryString = "";
-                char[] categoryCharArray = categoryString.ToCharArray();
-                bool isAdded = false;
-                for(int i = 0; i < categoryCharArray.Length-2; i++)
+                double price = Convert.ToDouble(PriceText);
+                ItemToSell newItem = new ItemToSell(ItemPhoto, categoryString, SizeText, StateText, price, NameText);
+            });
+
+            addPhotoCommand = new RelayCommand(o =>
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == true)
                 {
-                    if(categoryString[i] == ':')
-                        isAdded = true;
-                    if(isAdded)
-                        finalCategoryString += categoryString[i + 2];
+                    Uri fileUri = new Uri(openFileDialog.FileName);
+                    ItemPhoto = new BitmapImage(fileUri);
                 }
-                Trace.WriteLine("finalCategoryString = " + finalCategoryString);*/
             });
         }
 
-        private string size, state, name, price, category;
+        private ImageSource _ItemPhoto;
+
+        public ImageSource ItemPhoto
+        {
+            get { return _ItemPhoto; }
+            set
+            {
+                _ItemPhoto = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string size, state, name, price, category="Mężczyzna";
         public string getCategory
         {
             get { return this.category; }
